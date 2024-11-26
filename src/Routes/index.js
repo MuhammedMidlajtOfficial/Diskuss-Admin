f2b9c919eeb5bb8b25eefdde7c324297adf21702
Tushar-Gedam <tushargedam3@gmail.com>
2024-11-26 18:00:45 +0530

    created api to fetch all referral

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,17 +1,39 @@
+<<<<<<< Updated upstream
 const express = require('express');
 const overviewRouter = require("./Dashboard/overviewRouter")
+=======
+const express = require("express");
+const overviewRouter = require("./Dashboard/overviewRouter");
+const userRouter = require("./UserManagegent/userRouter");
+const referralRouter = require("./Referral/referralRouter");
+>>>>>>> Stashed changes
 
 const router = express.Router();
 
 const defaultRoutes = [
+<<<<<<< Updated upstream
   {
     path: "/dashboard",
     route: overviewRouter,
   },
+=======
+    {
+        path: "/dashboard",
+        route: overviewRouter,
+    },
+    {
+        path: "/user",
+        route: userRouter,
+    },
+    {
+        path: "/referal",
+        route: referralRouter,
+    },
+>>>>>>> Stashed changes
 ];
 
 defaultRoutes.forEach((route) => {
-  router.use(route.path, route.route);
+    router.use(route.path, route.route);
 });
 
 module.exports = router;
--- src/models/individualUser.js
+++ src/models/individualUser.js
@@ -1,81 +1,85 @@
 const mongoose = require("mongoose");
 
+const individualUserSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        isSubscribed: {
+            type: Boolean,
+            default: false,
+        },
+        cardNo: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        image: {
+            type: String,
+            default: "",
+        },
+        role: {
+            type: String,
+            default: "",
+        },
+        name: {
+            type: String,
+            default: "",
+        },
+        website: {
+            type: String,
+            default: "",
+        },
+        phnNumber: {
+            type: String,
+            default: "",
+        },
+        address: {
+            type: String,
+            default: "",
+        },
+        contacts: {
+            type: Array,
+            default: [],
+        },
+        meetings: [
+            {
+                type: String,
+                ref: "Meeting", // Reference to Meeting model
+                required: false,
+            },
+        ],
+        socialMedia: {
+            whatsappNo: {
+                type: String,
+                default: "",
+            },
+            facebookLink: {
+                type: String,
+                default: "",
+            },
+            instagramLink: {
+                type: String,
+                default: "",
+            },
+            twitterLink: {
+                type: String,
+                default: "",
+            },
+        },
+    },
+    { timestamps: true }
+);
 
-const individualUserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  isSubscribed: {
-    type: Boolean,
-    default: false,
-  },
-  cardNo: {
-    type: Number,
-    required: true,
-    default : 0
-  },
-  image: {
-    type:String,
-    default : ''
-  },
-  role: {
-    type:String,
-    default : ''
-  },
-  name: {
-    type:String,
-    default : ''
-  },
-  website: {
-    type:String,
-    default : ''
-  },
-  phnNumber: {
-    type: String,
-    default: '',
-  },
-  address: {
-    type:String,
-    default : ''
-  },
-  contacts : {
-    type : Array,
-    default : []
-  },
-  meetings : [{
-    type: String,
-    ref: "Meeting", // Reference to Meeting model
-    required: false,
-  }],
-  socialMedia: {
-    whatsappNo: {
-      type:String,
-      default : ''
-    },
-    facebookLink: {
-      type:String,
-      default : ''
-    },
-    instagramLink: {
-      type:String,
-      default : ''
-    },
-    twitterLink: {
-      type:String,
-      default : ''
-    },
-  }
-} ,{ timestamps: true });
-  
-  
-  module.exports.individualUser = mongoose.model('User', individualUserSchema);
-  
+const individualUser = mongoose.model("User", individualUserSchema);
+
+module.exports = { individualUser };
