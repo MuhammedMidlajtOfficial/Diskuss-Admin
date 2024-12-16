271f2d66d66badfe5e74c1f934e3b30a3273d7d3
Naren <deadlyassassin2003@gmail.com>
2024-12-16 14:20:22 +0530

    removing head

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,12 +4,9 @@ const userRouter = require("./UserManagegent/userRouter");
 const referralRouter = require("./Referral/referralRouter");
 const fcmRouter = require("./FcmRouter/fcmRouter");
 const SubscriptionPlan = require("./Subscription/subscription");
-<<<<<<< HEAD
-=======
 const PaymentHistory = require("./Subscription/userSubscription");
 const EmployeeRouter = require("./Employee/employeeRouter")
 const invoiceRouter = require("./Invoice/invoiceRouter");
->>>>>>> 49f557cb0fa4535aa5c6836b736378d6fd1786db
 
 const router = express.Router();
 
