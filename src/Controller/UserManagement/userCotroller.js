ffb7b3d29ac2738e71910342b9672516e17703a8
MidlajLevonTechno <muhammedmidlaj236@gmail.com>
2024-12-17 16:54:32 +0530

    enterprise view update

--- src/Controller/UserManagement/userCotroller.js
+++ src/Controller/UserManagement/userCotroller.js
@@ -612,3 +612,22 @@ module.exports.getEnterpriseUser = async (req, res) => {
     return res.status(500).json({ message: 'Server error' });
   }
 };
+
+module.exports.getEnterpriseUserById = async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    // Check if the user exists in the EnterpriseUser collection
+    const enterpriseUserExist = await enterpriseUser.findById(userId).populate('empId')
+    if (enterpriseUserExist) {
+      return res.status(200).json({ userData: enterpriseUserExist, userType: 'enterprise' });
+    }
+
+    // If no user is found in any collection
+    return res.status(404).json({ message: 'User not found' });
+
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return res.status(500).json({ message: 'Server error' });
+  }
+};
