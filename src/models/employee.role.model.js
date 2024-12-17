22d13cac082160484b14457f11a9831f254ab6dd
Abhishek Srivastav <abhisheksrivastavofficial@gmail.com>
2024-12-17 18:20:45 +0530

    refactor: Changed collection for employee category & roles to admin.category & admin.roles

--- src/models/employee.role.model.js
+++ src/models/employee.role.model.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const EmployeeRoleSchema = new mongoose.Schema({
     roleName: { type: String, required: true },
     isActive: { type: Boolean, default: false },
-}, { timestamps: true });
+}, { timestamps: true,
+    collection: 'admin.roles'
+ });
 
 module.exports = mongoose.model('EmployeeRole', EmployeeRoleSchema);
