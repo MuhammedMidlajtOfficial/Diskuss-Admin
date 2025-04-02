36ec0ecc7142dfd36a008a0a71f8fa380ab500bd
Rajanit Nagajibhai Navapara <51814458+rajanitnavapara@users.noreply.github.com>
2025-04-02 15:53:02 +0530

    Update index.js for testing

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use("/api/v1", router)
 
+app.use('/', (req,res) => {
+    res.json("Welcom to KC Admin API");
+});
+
 app.use((err, req, res, next) => {
   res.locals.error = err;
   console.log(err);
