28d4b0beed61384a5d3d984c47461b8bf4a672e6
Rajanit Nagajibhai Navapara <51814458+rajanitnavapara@users.noreply.github.com>
2025-04-02 15:55:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ const { sendNotificationsForOldRecords, notifyIncompleteContacts } = require("./
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.use("/api/v1", router)
-
-app.use('/', (req,res) => {
+app.use('/api', (req,res) => {
     res.json("Welcom to KC Admin API");
 });
+app.use("/api/v1", router)
+
 
 app.use((err, req, res, next) => {
   res.locals.error = err;
